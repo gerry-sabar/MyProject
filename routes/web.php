7ce9e9a98c12f2441bb51c01c229549ffbc8303b
Gerry <gerrysabar@gmail.com>
2020-04-09 19:26:14 +0700

    materi 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testing', function () {
+    return view('testing');
+});
+
+Route::get('/pricing', 'PricingController@index');
+Route::get('/manual', 'ManualController@index');
